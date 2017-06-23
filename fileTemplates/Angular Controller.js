1fa294657129d55cb3900eee81377eb8fe639fa9
Chad Glendenin <cglendenin@chooseenergy.com>
2017-06-22 20:52:26 -0500

    IU-2017.1.4 <cglendenin@Chads-MacBook-Pro.local Create baseRefactoring.xml, Angular Controller.js, markdown.xml, IntelliLang.xml, ide.general.xml, handangoCodeFormatter.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, git.xml, other.xml, Default.xml, Default-ccg.icls, AngularJS .xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, OpenJPA.xml, gradle.run.settings.xml, code.style.schemes.xml, colors.scheme.xml, path.macros.xml, databaseDrivers.xml, github_settings.xml, templates.xml, Default.xml, JavaScript.xml, parameter.hints.xml, jdk.table.xml, debugger.renderers.xml, laf.xml, Default _1_.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, keymap.xml, Angular Factory.js, editor.xml, usageView.xml, applicationLibraries.xml, debugger.xml

---
+++ fileTemplates/Angular Controller.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  
+  angular
+    .module('$moduleName')
+    .controller('$controllerName', function ($injectables) {
+      $END
+    });
+}());
---
+++ fileTemplates/Angular Factory.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+  
+  angular
+    .module('$moduleName')
+    .factory('$factoryName', function ($injectables) {
+      $END
+    });
+}());
